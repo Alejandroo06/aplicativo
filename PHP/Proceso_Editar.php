6d0b5a6b1d47f5c5423734e98f82eaa7f92e54b8
jhonfredd <jhonfredyrrada@gmail.com>
2022-09-11 14:01:40 -0500

    modificaciones de editar

--- PHP/Proceso_Editar.php
+++ PHP/Proceso_Editar.php
@@ -15,20 +15,13 @@
     $numero_placa = $_POST["numero_placa"];
     $destino_producto  = $_POST["destino_producto"];
     $foto=addslashes(file_get_contents($_FILES['soporte_producto']['tmp_name']));
-    $created = $red['created_at'];
-    print($created);
+    $created_at = $red['created_at'];
     $updated_at = date('Y-m-d h:i:s A');
     $nombre_cliente = $_POST["nombre_cliente"];
     $cedula_cliente = $_POST["cedula_cliente"];
 
-    $query = "UPDATE ingreso_seguimiento SET fecha_ingreso ='$fecha_ingreso', nombre_conductor ='$nombre_conductor' , tipo_producto = '$tipo_producto', foto = '$foto', numero_placa='$numero_placa, destino_producto='$destino_producto', created_at='$created_at',updated_at = '$update_at',nombre_cliente,cedula_cliente WHERE id='$id'";
-
-
-    $query = "UPDATE ingreso_seguimiento SET nombre_producto='$nombre_producto',  WHERE id = '$id'";
-    $resultado = mysqli_query($conexion,$query);
-
+    $query = "UPDATE ingreso_seguimiento SET fecha_ingreso='$fecha_ingreso', nombre_conductor='$nombre_conductor' , tipo_producto= '$tipo_producto', foto='$foto', numero_placa='$numero_placa', destino_producto='$destino_producto', created_at='$created_at',updated_at='$updated_at',nombre_cliente='$nombre_cliente',cedula_cliente='$cedula_cliente' WHERE id='$id'";
     $ejecutar = mysqli_query($conexion,$query);
-
     if($ejecutar){
         echo '
             <script>
@@ -40,7 +33,7 @@
         echo '
         <script>
            alert("Por favor intentalo de nuevo");
-           window.location = "Registro_Producto.php";
+           window.location = "EditarIngresoSeguimiento.php";
         </script>   
         ';
     }
